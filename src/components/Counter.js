7bbdcdcc955da0c9533f18efab7cc4386f1ee3f1
Rob Brander <rob.brander@gmail.com>
2015-11-10 14:28:40 -0500

    Introduce stateless arrow function components

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,22 +1,13 @@
-import React, { Component, PropTypes } from 'react';
+import React from 'react';
 
-class Counter extends Component {
-  static propTypes = {
-    counter: PropTypes.number.isRequired,
-    increment: PropTypes.func.isRequired,
-    decrement: PropTypes.func.isRequired,
-  };
-
-  render() {
-    const { counter, increment, decrement } = this.props;
-    return (
-      <div>
-        <button onClick={ increment }>Increment</button>
-        { ' ' + counter + ' ' }
-        <button onClick={ decrement }>Decrement</button>
-      </div>
-    );
-  }
-}
+const Counter = ({ counter, increment, decrement }) => {
+  return (
+    <div>
+      <button onClick={ increment }>Increment</button>
+      { ` ${ counter } ` }
+      <button onClick={ decrement }>Decrement</button>
+    </div>
+  );
+};
 
 export default Counter;
