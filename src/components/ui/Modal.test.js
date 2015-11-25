de5b06d848151ffa649c4a4a8b40a4f80dfe2d10
Neil Fenton <neilff@gmail.com>
2015-11-25 16:14:48 -0500

    modified npm script for test:watch, and added test utils

---
+++ src/components/ui/Modal.test.js
@@ -0,0 +1,26 @@
+import assert from 'assert';
+import jsdom from 'jsdom';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Modal from './Modal';
+
+describe('Modal Component', () => {
+  beforeEach(() => {
+    global.document = jsdom.jsdom();
+    global.window = document.defaultView;
+  });
+
+  it('should create a modal', () => {
+    const props = {
+      children: 'hello world',
+    };
+
+    const node = document.createElement('div');
+    const modal = ReactDOM.render(<div><Modal { ...props } /></div>, node);
+
+    assert(modal.childNodes[0].innerHTML === 'hello world');
+  });
+});
+
