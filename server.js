4a5152de7619a8ca45e7fccb9f8ef66c87cb99e5
Seth Davenport <seth@rangle.io>
2016-01-04 15:23:43 -0500

    Use serve-webpack-client to serve the client in dev and prod modes.

--- server.js
+++ server.js
@@ -1,29 +1,17 @@
 const express = require('express');
-const webpack = require('webpack');
 const winston = require('winston');
 const chalk = require('chalk');
+const path = require('path');
+const serveWebpackClient = require('serve-webpack-client');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-if (process.env.NODE_ENV !== 'production') {
-  const config = require('./webpack.config');
-  const compiler = webpack(config);
-
-  winston.info('Bundling webpack... Please wait.');
-
-  app.use(require('webpack-dev-middleware')(compiler, {
-    publicPath: config.output.publicPath,
-    stats: {
-      colors: true,
-      reasons: true,
-    },
-  }));
-
-  app.use(require('webpack-hot-middleware')(compiler));
-}
-
-app.get('*', express.static('dist'));
+app.use(serveWebpackClient({
+  distPath: path.join(__dirname, 'dist'),
+  indexFileName: 'index.html',
+  webpackConfig: require('./webpack.config'),
+}));
 
 app.listen(PORT, (err) => {
   if (err) {
