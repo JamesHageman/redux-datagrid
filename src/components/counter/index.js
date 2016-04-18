63d20b16cc80959a6856dfd40ce51a4d76c53517
Jeremy Casey <jeremy8883@gmail.com>
2016-04-18 10:09:09 -0400

    Add tests for counter component

--- src/components/counter/index.js
+++ src/components/counter/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import Button from './button';
+import Button from '../button';
 
 function Counter({ counter, increment, decrement }) {
   return (
     <div className="flex">
-      <Button className="bg-black col-2"
+      <Button data-ref="decrementButton" className="bg-black col-2"
         onClick={ decrement }>
         -
       </Button>
 
-      <div className="flex-auto center h1">
+      <div data-ref="result" className="flex-auto center h1">
         { counter }
       </div>
 
-      <Button className="col-2"
+      <Button data-ref="incrementButton" className="col-2"
         onClick={ increment }>
         +
       </Button>
