13a82ff2f294c16a402926766dc865c44bc26169
Jeremy Casey <jeremy8883@gmail.com>
2016-04-18 11:05:29 -0400

    Change modal tests to use enzyme in favour of jsdom

--- src/components/modal/modal.test.js
+++ src/components/modal/modal.test.js
@@ -1,25 +1,13 @@
 import assert from 'assert';
-import jsdom from 'jsdom';
+import { render } from 'enzyme';
 
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 import Modal from './modal';
 
 describe('Modal Component', () => {
-  beforeEach(() => {
-    global.document = jsdom.jsdom();
-    global.window = document.defaultView;
-  });
-
   it('should create a modal', () => {
-    const props = {
-      children: 'hello world',
-    };
-
-    const node = document.createElement('div');
-    const modal = ReactDOM.render(<div><Modal { ...props } /></div>, node);
-
-    assert(modal.childNodes[0].innerHTML === 'hello world');
+    const wrapper = render(<Modal>Hello world</Modal>);
+    assert.equal(wrapper.text(), 'Hello world');
   });
 });
