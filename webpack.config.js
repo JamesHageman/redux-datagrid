b2a4ef5568f0dc3279b1be51eccf6fd732676bb1
James Hageman <jamesd@hageman.ca>
2016-05-19 15:34:33 -0400

    Remove old example

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,11 @@ module.exports = {
   },
 
   plugins: [
+    new webpack.DefinePlugin({
+      __DEV__: process.env.NODE_ENV !== 'production',
+      __PRODUCTION__: process.env.NODE_ENV === 'production',
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
     new HtmlWebpackPlugin({
       template: './site/index.html',
       inject: 'body',
